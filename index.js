2d0add32bc02fd309037fc55d00b091d9f957b27
robrechtme <robrechtmeersman@hotmail.com>
2022-08-01 20:41:21 +0200

    fix: add missing deps and peer deps

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ module.exports = {
   plugins: ["prettier", "@typescript-eslint"],
   rules: {
     "@next/next/no-img-element": "off",
-    "@typescript-eslint/no-unused-vars": "error",
     "@typescript-eslint/consistent-type-definitions": ["error", "interface"],
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/no-unused-vars": "error",
     "arrow-body-style": "error",
-    "object-shorthand": "error",
     "import/order": [
       "error",
       {
@@ -23,7 +23,8 @@ module.exports = {
         alphabetize: { order: "asc" },
       },
     ],
-    "prettier/prettier": "error",
     "jsx-uses-react": "off",
+    "object-shorthand": "error",
+    "prettier/prettier": "error",
   },
 };
